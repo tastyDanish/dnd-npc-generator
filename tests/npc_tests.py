579936e327a129c8dd82fb88558113024d4a553b
Peter Lansdaal <pndaal@gmail.com>
2019-07-04 19:54:10 -0700

    updating npc tests for new format

--- tests/npc_tests.py
+++ tests/npc_tests.py
@@ -8,7 +8,8 @@ class TestApp(unittest.TestCase):
 
     # Generate an NPC to test against
     def setUp(self):
-        self.test_npc = NPC(9)
+        self.test_npc = NPC()
+        self.test_npc.generate_npc()
         print(self.test_npc)
 
     def tearDown(self):
@@ -16,17 +17,18 @@ class TestApp(unittest.TestCase):
 
     # 1. test if the proficiency bonus is calculated right
     def test_npc_prof_bonus(self):
-        expected_prof_bonus = 4
+        expected_prof_bonus = 2
 
         assert self.test_npc.prof_bonus == expected_prof_bonus
 
     def test_npc_npc_name(self):
         expected_name_type = str
 
-        actual_name_type = type(self.test_npc.name)
+        actual_name_type = type(self.test_npc.name.value)
 
         assert actual_name_type == expected_name_type
 
     def test_input_vals(self):
-        input_npc = NPC(level=9, race='Human', archetype='Bulky')
+        input_npc = NPC()
+        input_npc.generate_npc(level=9, race='Human', archetype='Bulky')
         print(input_npc)
